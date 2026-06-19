2c52edcb355d223f7c776d49fe7587e7ede1cffa
halojerry <1041435346@qq.com>
2026-06-19 22:21:25 +0800

    fix(ci): rename web-cli mock assets from aionui-web-* to pounding-web-*

--- scripts/create-mock-release-artifacts.sh
+++ scripts/create-mock-release-artifacts.sh
@@ -92,7 +92,7 @@ WEB_PLATFORMS=(
 for plat in "${WEB_PLATFORMS[@]}"; do
   dir="$ARTIFACTS_DIR/web-cli-${plat}"
   mkdir -p "$dir"
-  tarball="aionui-web-1.0.0-${plat}.tar.gz"
+  tarball="pounding-web-1.0.0-${plat}.tar.gz"
   touch "$dir/$tarball"
   # Produce a deterministic fake SHA256 file in the expected format:
   # "<64 hex chars>  <filename>"
